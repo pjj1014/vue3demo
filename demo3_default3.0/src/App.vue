b7b5c4ac72187f2ea422ed4c26f28fdda158ced5
pjj1014 <447198437@qq.com>
2023-11-07 16:15:17 +0800

    1

--- demo3_default3.0/src/App.vue
+++ demo3_default3.0/src/App.vue
@@ -1,16 +1,30 @@
 <template>
-  <Demo>
-  </Demo>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-
-import Demo from './components/Demo.vue'
-export default {
-  components: { Demo },
-  name: 'App',
-  
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
 
+nav {
+  padding: 30px;
+}
 
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- demo3_default3.0/src/main.js
+++ demo3_default3.0/src/main.js
@@ -1,9 +1,11 @@
 //引入的不再是 Vue 构造函数了，引入的是一个名为 createApp的工厂函数
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
 //创建应用实例对象--app(类似于之前 Vue2 中的 vm ,但 app 比 vm 更“轻”)
-const app = createApp(App)
+const app = createApp(App).use(store).use(router)
 
 //挂载
 app.mount('#app')
